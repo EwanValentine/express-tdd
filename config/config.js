bcb398fa7f5d3a711718f72e194b6cb8c81450fd
Ewan Valentine <ewan.valentine89@gmail.com>
2017-08-08 21:50:30 +0100

    ADDED datastore connection

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB_HOST: 'localhost:27017',
+  PORT: 3000,
+}
--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const app = express()
-const config = require('./app/config/config')
+const config = require('./config/config')
 
+// Datastore set-up
+mongoose.connect(config.DB_HOST)
+
+// On connection
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose default connection open to ' + config.db)
+})
+
+const port = process.env.PORT || config.PORT
+
+// Middleware and logging set-up
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({'extended':'true'}))
 app.use(bodyParser.json())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 app.use(methodOverride())
+
+// Listen on port
+app.listen(port, err => {
+  if(err) throw err
+  console.log(`App listening on port ${config.PORT}`)
+})
