35530efbd2f4cd49eebeac0855fb4ff29acc09d1
Ewan Valentine <ewan.valentine89@gmail.com>
2017-08-15 00:18:28 +0100

    ADDED some missing doc blocks

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,10 @@
-function Product() {
-  this.products = [
+/**
+ * Product
+ *
+ * @param {Array} products
+ */
+function Product(products = []) {
+  this.products = products || [
     { sku: 'A', price: 50, special: { quantity: 3, price: 130 } },
     { sku: 'B', price: 30, special: { quantity: 2, price: 45 } },
     { sku: 'C', price: 20, },
@@ -7,10 +12,21 @@ function Product() {
   ]
 }
 
+/**
+ * find
+ *
+ * @param {String} sku
+ * @return {Array}
+ */
 Product.prototype.find = function (sku) {
   return Promise.resolve(this.products.find(item => item.sku === sku))
 }
 
+/**
+ * findAll
+ *
+ * @return {Array}
+ */
 Product.prototype.findAll = function() {
   return Promise.resolve(this.products)
 }
